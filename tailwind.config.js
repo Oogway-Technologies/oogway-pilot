a4a3074d4b378612ce5a9d972c579126c1abf6d4
Evan Jones <evan.a.jones3@gmail.com>
2022-01-14 12:08:50 -0500

    extend spacing and colors; add screens and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,56 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      sans: ["inter"],
+      serif: ["inter"],
+      display: ["inter"],
+      body: ["inter"],
+    },
+    fontFeatureSettings: {
+      numeric: ["tnum", "salt", "ss02"],
+    },
+    extend: {
+      spacing: {
+        xs: "4px",
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "32px",
+        xxl: "64px",
+      },
+      colors: {
+        transparent: "transparent",
+        primary: "#7041EE",
+        secondary: "#EA7979",
+        black: "#000000",
+        white: "#FFFFFF",
+        neutral: {
+          50: "#F4F4F4",
+          100: "#E5E5E5",
+          150: "#D8D8D8",
+          300: "#BFBFBF",
+          700: "#535353",
+          800: "#1A1A1A",
+        },
+        error: {
+          50: "#FFA3A3",
+          150: "#FF7A7A",
+          300: "#FF3D3D",
+          500: "#EA0000",
+          600: "#BC0000",
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-font-inter")],
+};
