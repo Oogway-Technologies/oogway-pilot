01118ba9d7e033420ef815efa41d2e8d1ec68d26
Federico Campeotto <fede.campe@gmail.com>
2022-01-16 15:29:21 -0500

    Add login functionalities with no logout on refresh using Firebase.
    This commit adds login functionality using email and password.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import {auth} from '../firebase';
+// import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
+  // Can use the hook to get user information
+  // const [user] = useAuthState(auth);
+
   return (
     <div className="">
       <Head>
