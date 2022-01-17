0078ce09bfd24fd4b1b27a7df31c5676a20e62c7
Evan Jones <evan.a.jones3@gmail.com>
2022-01-17 10:30:12 -0500

    remove unused dependencies and vars

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import { useTheme } from 'next-themes';
 
 export default function Home() {
-    const {theme, setTheme} = useTheme()
-
     return (
     <div className="">
       <Head>
