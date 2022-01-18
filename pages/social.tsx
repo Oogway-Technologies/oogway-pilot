3f8eb709c0c5afd2aadc7757f90bf4a721b36fd6
Evan Jones <evan.a.jones3@gmail.com>
2022-01-18 13:15:37 -0500

    create empty social page

---
+++ pages/social.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+
+export default function Social() {
+    // Can use the hook to get user information
+    // const [user] = useAuthState(auth);
+  
+    return (
+      <div className="">
+        <Head>
+          <title>Oogway | Social - Wisdom of the crowd</title>
+        </Head>
+      
+        <h1>This will be the social page</h1>
+      </div>
+    )
+}
+  
