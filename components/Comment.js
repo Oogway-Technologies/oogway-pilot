c72b705eb988052b4f60406b86d77d228d24af6f
Federico Campeotto <fede.campe@gmail.com>
2022-01-20 15:26:32 -0500

    TEMPORARY COMMIT - ADDING COMMENTS TO POSTS

---
+++ components/Comment.js
@@ -0,0 +1,11 @@
+function Comment({ comment }) {
+  return (
+    <div>
+      {console.log("hi---")}
+      {console.log(comment)}
+      <h1 className="text-red-800">hi</h1>
+    </div>
+  );
+}
+
+export default Comment;
