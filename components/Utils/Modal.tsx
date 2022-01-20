14b0f86c9758f3b47bc916ac2ee24833a35fa860
Evan Jones <evan.a.jones3@gmail.com>
2022-01-20 01:15:29 -0500

    Add generic modal utility component

---
+++ components/Utils/Modal.tsx
@@ -0,0 +1,58 @@
+import { Dialog, Transition } from '@headlessui/react';
+import React, { Fragment, ReactNode } from 'react';
+
+type ModalProps = {
+    children: ReactNode,
+    show: boolean,
+    onClose: ((value: boolean) => void)
+};
+
+const Modal: React.FC<ModalProps> = ({ children: content, show, onClose }) => {  
+    return (
+        <Transition appear show={show} as={Fragment}>
+            <Dialog 
+                className="fixed inset-0 z-10 overflow-y-auto flex  justify-center items-center"
+                as="div"
+                onClose={onClose}
+            >
+                <div className="flex justify-center px-4 text-center">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Dialog.Overlay className="fixed inset-0 bg-neutralDark-300/75" />
+                    </Transition.Child>
+
+                    {/* This element is to trick the browser into centering the modal contents. */}
+                    <span
+                        className="inline-block align-middle"
+                        aria-hidden="true"
+                    >
+                        &#8203;
+                    </span>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <div className="justify-center items-center max-w-6xl p-6 my-8 overflow-hidden text-left
+                        transition-all transform bg-white dark:bg-neutralDark-500 shadow-xl rounded-2xl">
+                            {content}
+                        </div>
+                    </Transition.Child>
+                </div>       
+        </Dialog>
+      </Transition>
+    );
+};
+
+export default Modal;
