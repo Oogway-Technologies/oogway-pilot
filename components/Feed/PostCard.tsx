c3d5a9633895f1872cc3105545c602b38ef2a2d9
Evan Jones <evan.a.jones3@gmail.com>
2022-01-20 23:38:17 -0500

    refactor styles

--- components/Feed/PostCard.tsx
+++ components/Feed/PostCard.tsx
@@ -5,21 +5,26 @@ import { UilComment, UilThumbsUp, UilUpload, UilBookmark, UilEllipsisH } from '@
 import moment from 'moment'
 import Button from '../Utils/Button';
 
-// Styling
-const postCardClass = "flex flex-col px-md pt-md pb-sm rounded-md \
-    shadow-sm shadow-black/20 dark:shadow-black/60 dark:bg-neutralDark-500"
-const headerClass = "inline-block md:inline-flex relative text-sm text-neutral-700 dark:text-neutralDark-150 \
-    space-x-1 items-center"
-const bodyClass = "flex flex-col ml-xl"
-const engagementClass = "flex px-md mt-[36px] pb-md ml-xl w-3/5 justify-between text-sm"
-const engagementButtonClass = "inline-flex items-center space-x-2 text-neutral-700 dark:text-neutralDark-150 \
-    hover:text-neutral-800 dark:hover:text-neutralDark-50 focus:text-neutral-800 dark:focus:text-neutralDark-50 \
-    active:text-primary"
 
-// Helper components
-const avatarURL =
-        "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
-  
+// Styling
+const postCardClass = {
+    card: "flex flex-col px-md pt-md pb-sm rounded-md shadow-sm shadow-black/20 dark:shadow-black/60 \
+        dark:bg-neutralDark-500",
+    header: "inline-block md:inline-flex relative text-sm text-neutral-700 dark:text-neutralDark-150 \
+        space-x-1 items-center",
+    headerMobileRowOne: "inline-flex items-center md:flex",
+    headerMobileRowTwo: "inline-flex items-center  pl-[46px] md:pl-0 text-xs md:text-sm md:flex",
+    body: "flex flex-col ml-xl",
+    bodyQuestion: "font-bold mb-[8px] text-md text-neutral-800 dark:text-neutralDark-50",
+    bodyDescription: "text-neutral-700 dark:text-neutralDark-150 text-sm",
+    engagementBar: "flex px-md mt-[36px] pb-md ml-xl w-3/5 justify-between text-sm",
+    engagementButton: "inline-flex items-center space-x-2 text-neutral-700 dark:text-neutralDark-150 \
+        hover:text-neutral-800 dark:hover:text-neutralDark-50 focus:text-neutral-800 dark:focus:text-neutralDark-50 \
+        active:text-primary"
+
+}
+
+// Helper vars
 const bull = (
     <Box
       component="span"
@@ -59,18 +64,20 @@ interface PostProps {
 };
 
 const PostCard: React.FC<PostProps> = ({ name, message, email, postImage, image, timestamp }) => {
-      
+    const avatarURL =
+        "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+
     // Placeholder for hooks until they are added
     const needsHook = () => {
         alert('This button needs a hook!')
     }
 
     return (
-        <Card className={postCardClass}>
+        <Card className={postCardClass.card}>
             {/* TODO: Refactor header into it's own component */}
-            <div className={headerClass}>
+            <div className={postCardClass.header}>
                 {/* Split into two rows on mobile */}
-                <div className="inline-flex items-center md:flex">
+                <div className={postCardClass.headerMobileRowOne}>
                     {/* Avatar */}
                     <Avatar
                         onClick={needsHook}
@@ -82,13 +89,16 @@ const PostCard: React.FC<PostProps> = ({ name, message, email, postImage, image,
                     <span className="pl-sm">{name ? name : email}</span>
                 </div>
 
-                <div className="inline-flex items-center  pl-[46px] md:pl-0 text-xs md:text-sm md:flex">
+                <div className={postCardClass.headerMobileRowTwo}>
                     {/* Number of replies */}
                     {/* TODO: interpolate number below */}
                     <span className="hidden md:flex">{bull}</span> <p className='inline-flex'>0 <span className="hidden md:flex md:ml-1">Answers</span></p>
+                    {/* TODO: interpolate post category below */}
                     {bull} <p className="font-bold text-primary">Education</p>
                     {bull}
-                    {/* Time stamp */}
+                    {/* Time stamp 
+                        TODO: Make into separate utillity
+                    */}
                     {
                         timestamp ? (
                             <p>
@@ -107,11 +117,11 @@ const PostCard: React.FC<PostProps> = ({ name, message, email, postImage, image,
                 </button>
             </div>
 
-            <CardContent className={bodyClass}>
-                <Typography component={'h4'} className="font-bold mb-[8px] text-md text-neutral-800 dark:text-neutralDark-50">
+            <CardContent className={postCardClass.body}>
+                <Typography component={'h4'} className={postCardClass.bodyQuestion}>
                     Insert question here, make description the body.
                 </Typography>
-                <Typography className="text-neutral-700 dark:text-neutralDark-150 text-sm">
+                <Typography className={postCardClass.bodyDescription}>
                 {message}
                 </Typography>
             </CardContent>
@@ -124,12 +134,12 @@ const PostCard: React.FC<PostProps> = ({ name, message, email, postImage, image,
             )}
 
             {/* Engagement */}
-            <Box className={engagementClass}>
+            <Box className={postCardClass.engagementBar}>
                 {
                     engagementItems.map((item, idx) => (
                         <Button
                             key={idx}
-                            addStyle={engagementButtonClass}
+                            addStyle={postCardClass.engagementButton}
                             type='button'
                             onClick={needsHook}
                             icon={item.icon}
