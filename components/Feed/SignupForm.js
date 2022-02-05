33571f97fe3a16b26af389bf8d29ee5a96ea9af1
Steven Tegnelia <steventegnelia@Stevens-MacBook-Air.local>
2022-02-04 18:18:53 -0700

    button sizing

--- components/Feed/SignupForm.js
+++ components/Feed/SignupForm.js
@@ -82,7 +82,7 @@ export default function SignUpForm({ closeSignupModal }) {
 }
 
 const loginButtonStyle =
-  "rounded-[20px] p-sm md:px-md md:space-x-2 border-solid border-transparent\
+  "rounded-[20px] p-sm my-5  w-2/4 mx-2 justify-center px-1.5 py-3 md:px-md md:space-x-2 border-solid border-transparent\
 text-neutral-700 dark:text-neutralDark-150 \
 bg-violet-600 text-white \
 hover:font-bold active:font-bold dark:hover:font-bold dark:active:font-bold hover:text-black \
@@ -90,7 +90,7 @@ hover:bg-neutral-50 dark:hover:bg-neutralDark-300 active:bg-primary/20 dark:acti
 hover:text-neutral-700 dark:hover:text-neutralDark-150 active:text-primary dark:active:text-primaryDark";
 
 const cancelButtonStyle =
-  "rounded-[20px] p-sm md:px-md md:space-x-2 border-solid border-transparent\
+  "rounded-[20px] w-2/4 mx-2 px-1.5 py-3 justify-center align-middle content-center p-sm md:px-md md:space-x-2 border-solid border-transparent\
 text-neutral-700 dark:text-neutralDark-150 \
 bg-stone-300 text-slate-700 pr-3.5 \
 hover:font-bold active:font-bold dark:hover:font-bold dark:active:font-bold hover:text-black \
