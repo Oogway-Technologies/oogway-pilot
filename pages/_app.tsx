b657a1491a3489107cc680bc385875441fd6d746
Evan Jones <evan.a.jones3@gmail.com>
2022-02-07 23:40:09 -0500

    update db utils, pages auth routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,75 +1,54 @@
-import {useEffect} from 'react';
-import '../styles/globals.css';
-import Layout from '../components/Layout';
-import { ThemeProvider } from 'next-themes';
-import  type { AppProps} from 'next/app';
-import { RecoilRoot } from 'recoil';
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
-import { createUser, createUserProfile } from '../lib/db';
-import { formatUser, getRandomUsername, getRandomProfilePic } from '../lib/user';
-import Login from "./login";
-import Loading from "../components/Loading";
+import { useEffect } from 'react'
+import '../styles/globals.css'
+import Layout from '../components/Layout'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase'
+import { createUser } from '../lib/db'
+import { formatUser } from '../lib/user'
+import Login from './login'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // Read in config variables from environment
-  const appConfig = {
-    isDevelopment: process.env.NEXT_PUBLIC_DEV
-  }
+    // Read in config variables from environment
+    const appConfig = {
+        isDevelopment: process.env.NEXT_PUBLIC_DEV,
+    }
 
-  // Set up state
-  const [user, loading] = useAuthState(auth);
+    // Set up user state
+    const [user, loading] = useAuthState(auth)
 
-  // Run the first time the component is mounted
-  useEffect(() => {
-      if (user) {
-        // Format the user to store it into the DB
-        (async () => await formatUser(user))()
-        .then((userFormatted) => {
+    // Run the first time the component is mounted
+    useEffect(() => {
+        if (user) {
+            // Format the user to store it into the DB
+            ;(async () => await formatUser(user))().then((userFormatted) => {
+                // Extract everything but and discard the token
+                const { token, ...userWithoutToken } = userFormatted
 
-          // Extract everything but and discard the token
-          const { token, ...userWithoutToken } = userFormatted;
+                // Push the user to the DB
+                createUser(userFormatted.uid, userWithoutToken)
+            })
+        }
+    }, [user])
 
-          // Push the user to the DB
-          createUser(userFormatted.uid, userWithoutToken);
+    const AuthState = () => {
+        if (loading) return <Loading />
+        else if (!user) return <Login />
+        else return <Component {...pageProps} />
+    }
 
-          // Add default user profile
-          const defaultProfile = {
-            username: getRandomUsername(),
-            name: "",
-            lastName: "",
-            profilePic: getRandomProfilePic(),
-            bio: "",
-            location: "",
-            allowDM: false,
-            resetProfile: true,
-            uid: userWithoutToken.uid,
-          }
-          createUserProfile(userFormatted.uid, defaultProfile)
-        })
-      }
-  }, [user]);
-
-
-  // Only add login for production builds
-  if (!appConfig.isDevelopment) {
-    // If the authentication is loading (e.g., check token validity),
-    // return the loading screen
-    if (loading) return <Loading />
-  }  
-
-  // If the user is not logged-in, re-route to the Login page
-  if (!user) return <Login />
-
-  return (
-    <ThemeProvider attribute='class' enableSystem={true}>
-        <Layout>
-          <RecoilRoot>
-            <Component {...pageProps} />
-          </RecoilRoot>
-        </Layout>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider attribute="class" enableSystem={true}>
+            <Layout>
+                <RecoilRoot>
+                    <AuthState />
+                </RecoilRoot>
+            </Layout>
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
--- pages/login.js
+++ pages/login.js
@@ -1,40 +1,29 @@
-import { useState } from "react";
-import Head from "next/head";
-import Modal from "../components/Utils/Modal";
-import LoginForm from "../components/Login/LoginForm";
-import { loginDivs } from "../styles/login";
+import { useState } from 'react'
+import Head from 'next/head'
+import Modal from '../components/Utils/Modal'
+import { loginDivs } from '../styles/login'
+import AuthenticationAPI from '../components/Login/AuthenticationAPI'
 
 function Login() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true)
 
-  // Modal helper functions
-  const openModal = () => {
-    setIsOpen(true);
-  };
+    // Modal helper functions
+    const closeModal = () => {
+        setIsOpen(false)
+    }
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  return (
-    <div className={loginDivs.container}>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <div>
-        <button onClick={openModal}>Login</button>
-      </div>
-      <img
-        className={loginDivs.logo}
-        src='https://cdn-icons-png.flaticon.com/512/2395/2395608.png'
-      />
-      <Modal
-        children={<LoginForm closeModal={closeModal} />}
-        show={isOpen}
-        onClose={closeModal}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <Modal
+                children={<AuthenticationAPI closeModal={closeModal} />}
+                show={isOpen}
+                onClose={closeModal}
+            />
+        </div>
+    )
 }
 
-export default Login;
+export default Login
