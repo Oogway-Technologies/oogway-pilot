62b69e26f83a61ddde604b3734a072385126da02
Evan Jones <evan.a.jones3@gmail.com>
2022-02-09 12:02:04 -0500

    Fix nav links highlighting

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,48 +1,47 @@
-import Logo from "../Logo"
+import Logo from '../Logo'
 import Link from 'next/link'
-import SearchBar from './SearchBar';
-import AppsButton from './AppsButton';
-import NavLinks from './NavLinks';
-import UserDropdown from "./UserDropdown";
-import { headerClass } from "../../styles/header";
-
+import SearchBar from './SearchBar'
+import AppsButton from './AppsButton'
+import NavLinks from './NavLinks'
+import UserDropdown from './UserDropdown'
+import { headerClass } from '../../styles/header'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
-  return (
-    <div className={headerClass.div}>
-        {/* Top: Toolbar */}
-        <div 
-        className={headerClass.toolbar}>
-            {/* Left: Logo */}
-            <div 
-            className={headerClass.logo}>
-                <Link href="/" passHref>
-                    <a><Logo fill="currentColor"/></a>
-                </Link>
+    return (
+        <div className={headerClass.div}>
+            {/* Top: Toolbar */}
+            <div className={headerClass.toolbar}>
+                {/* Left: Logo */}
+                <div className={headerClass.logo}>
+                    <Link href="/" passHref>
+                        <a>
+                            <Logo fill="currentColor" />
+                        </a>
+                    </Link>
+                </div>
+
+                {/* Center: Search */}
+                <div className={headerClass.search}>
+                    <SearchBar placeholder="What's your question?" />
+                </div>
+
+                {/* Right: User */}
+                <div className={headerClass.user}>
+                    {/* Uncomment Apps when we have hook */}
+                    {/* <AppsButton /> */}
+                    <UserDropdown />
+                </div>
             </div>
-        
-            {/* Center: Search */}
-            <div 
-            className={headerClass.search}>
-                <SearchBar placeholder="What's your question?" />
-            </div>
-        
-            {/* Right: User */}
-            <div 
-            className={headerClass.user}>
-                {/* Uncomment Apps when we have hook */}
-                {/* <AppsButton /> */}
-                <UserDropdown />
+
+            {/* Bottom: Slug */}
+            <div className={headerClass.slug}>
+                <NavLinks listStyle={headerClass.slugList} />
             </div>
         </div>
+    )
+}
 
-        {/* Bottom: Slug */}
-        <div className={headerClass.slug}>
-            <NavLinks listStyle={headerClass.slugList}/>
-        </div>
-
-    </div>
-  );
-};
-
-export default Header;
+export default Header
