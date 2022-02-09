9f04ae8ac77bfdbc838e586c2c7f0d37bd191779
Evan Jones <evan.a.jones3@gmail.com>
2022-02-09 12:02:04 -0500

    Fix nav links highlighting

--- components/Header/NavLinks.tsx
+++ components/Header/NavLinks.tsx
@@ -4,45 +4,43 @@ import React from 'react'
 import { navLinksClass } from '../../styles/header'
 
 interface NavLinksProps {
-    links: Array<NavLink>,
+    links: Array<NavLink>
     listStyle: string
 }
 
 type NavLink = {
-    href: Array<string>,
+    href: Array<string>
     text: string
 }
 
 const NavLinks = ({ links, listStyle }: NavLinksProps) => {
     const router = useRouter()
-    // const MyLink = React.forwardRef((props, ref) => <Link passHref={ref} {...props}/> );
 
     return (
         <ul className={listStyle}>
-            {
-                links.map((link, idx) => {
-                    /** Warns about passing a prop to href. 
-                     * Might need to undo functional approach here an manually enumerate each link.
-                     * */
-                    return (
-                        
-                        <li key={idx} className={navLinksClass.li}>
-                            <Link href={link.href[0]} passHref> 
-                                <a 
-                                className={
-                                    `${link.href.includes(router.pathname.split("?")[0]) ? 
-                                    'text-primary font-bold underline underline-offset-8' : 
-                                    'text-neutral-700 dark:text-neutralDark-150'} 
-                                    text-xs pt-2 pb-[6px] md:text-base hover:font-bold`
-                                    }
-                                >
-                                    {link.text}
-                                </a>
-                            </Link>
-                        </li>
-                    )
-                })
-            }
+            {links.map((link, idx) => {
+                /** Warns about passing a prop to href.
+                 * Might need to undo functional approach here an manually enumerate each link.
+                 * */
+                return (
+                    <li key={idx} className={navLinksClass.li}>
+                        <Link href={link.href[0]} passHref>
+                            <a
+                                className={`${
+                                    link.href.includes(
+                                        router.pathname.split('?')[0]
+                                    )
+                                        ? 'text-primary font-bold underline underline-offset-8'
+                                        : 'text-neutral-700 dark:text-neutralDark-150'
+                                } 
+                                    text-xs pt-2 pb-[6px] md:text-base hover:font-bold`}
+                            >
+                                {link.text}
+                            </a>
+                        </Link>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
@@ -51,19 +49,19 @@ NavLinks.defaultProps = {
     links: [
         // TODO: Add as pages created
         {
-            href: ["/#"], // change to /Search when search page created
-            text: "Search"
+            href: ['/#'], // change to /Search when search page created
+            text: 'Search',
         },
         {
-            href: ["/social", "/"],
-            text: "Feed"
+            href: ['/feed/[id]'],
+            text: 'Feed',
         },
         {
-            href: ["/#"], // change to Friends when created
-            text: "Friends"
+            href: ['/#'], // change to Friends when created
+            text: 'Friends',
         },
     ],
-    listStyle: ''
-} 
+    listStyle: '',
+}
 
 export default NavLinks
