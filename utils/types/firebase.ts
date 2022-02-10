ef045027a795c7e88ec508fc2eafee7bd58081ba
Ihtisham Arif <im.ihtisham.arif@gmail.com>
2022-02-11 01:38:33 +0500

    PR updates

--- utils/types/firebase.ts
+++ utils/types/firebase.ts
@@ -8,3 +8,26 @@ export interface FirebaseUserProfile {
     lastSeen: string;
     name: string;
 }
+
+export interface FirebaseUser {
+    email: string;
+    expirationTime: string;
+    lastSeen: string;
+    name: string;
+    photoUrl: string;
+    provider: string;
+    resetProfile: boolean;
+    uid: string;
+}
+
+export interface FirebaseProfile {
+    bio: string;
+    dm: boolean;
+    lastName: string;
+    location: string;
+    name: string;
+    profilePic: string;
+    resetProfile: boolean;
+    uid: string;
+    username: string;
+}
