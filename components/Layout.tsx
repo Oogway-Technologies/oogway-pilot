8c68615230b3869e0183a8f8215120f931516654
Federico Campeotto <fede.campe@gmail.com>
2022-02-10 21:50:39 -0500

    Addressed PR comments

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect} from 'react'
 import Header from './Header/Header'
 import {useUser} from "@auth0/nextjs-auth0";
-import {userProfileState} from "../atoms/userProfile";
+import {userProfileState} from "../atoms/user";
 import {useSetRecoilState} from "recoil";
-import {getUserFromFirebase} from "../lib/userHelper";
+import {getOrCreateUserFromFirebase} from "../lib/userHelper";
 
 interface LayoutProps {
     children: React.ReactNode
@@ -11,20 +11,25 @@ interface LayoutProps {
 
 
 const Layout = ({children}: LayoutProps) => {
-    // useUser is used to get the current Auth0 user.
     const {user, isLoading} = useUser();
-    // using useSetRecoilState to get userProfileState state from recoil.
+
+    // Get userProfileState state from recoil.
     const setUserProfileState = useSetRecoilState(userProfileState);
 
-    // This useEffect will run everytime the page is reloaded, to fetch the user from the backend and it will set to recoil state.
+    // This useEffect will run everytime the page is reloaded
+    // to fetch the user profile information from the backend.
+    // It will then set the recoil state with the user profile information
+    // to make it available to each component.
     useEffect(() => {
-        // isLoading is completed and user is exists, getUserFromFirebase will get the returned user form the backend and set it to recoil state by setUserProfileState
+        // isLoading is completed (i.e., it is not loading anymoe) and user exists,
+        // getUserFromFirebase will get the returned user form the backend
+        // and set it to recoil state by setUserProfileState
         if (!isLoading && user) {
-            getUserFromFirebase(user).then((data) => {
+            getOrCreateUserFromFirebase(user).then((data) => {
                 setUserProfileState(data || {});
             })
         }
-    }, [isLoading])
+    }, [isLoading, user]);
 
     return (
         <div className="flex flex-col min-h-screen max-h-screen overflow-y-hidden">
--- lib/userHelper.ts
+++ lib/userHelper.ts
@@ -1,46 +1,57 @@
 import {UserProfile} from "@auth0/nextjs-auth0/src/frontend/use-user";
 import {db} from "../firebase";
-import {addDoc, collection, doc, getDocs, query, setDoc, where} from "firebase/firestore";
+import {addDoc, collection, doc, getDocs, getDoc, query, setDoc, where, serverTimestamp} from "firebase/firestore";
 import {FirebaseProfile, FirebaseUser} from "../utils/types/firebase";
 
-export const getUserFromFirebase = async (user: UserProfile) => {
+export const getOrCreateUserFromFirebase = async (user: UserProfile) => {
     try {
+        // Check if the Auth0-Firebase mapping is available
         const checkIfUserExists = await getDocs(query(collection(db, "auth0"), where("auth0", "==", user.sub)));
 
-        //creates user if not created.
+        // Create user if it doesn't already exist
         if (!checkIfUserExists.docs.length) {
-            // to create Auth0 of the new user.
+            // The mapping is not present:
+            // This is the first time ever the user logs into the app.
+            // Create Auth0 mapping for the new user
             const newAddedAuthRef = await addDoc(collection(db, 'auth0'), {
                 auth0: user.sub,
             })
-            // to create new user.
+
+            // Use the referenced doc id to add a new user and user profile.
+            // TODO: check if blockedUsers and posts are better as sub-collections.
             const newUser: FirebaseUser = {
                 email: user?.email || '',
-                expirationTime: '',
-                lastSeen: '',
+                lastSeen: serverTimestamp(),
                 name: user?.name || '',
-                photoUrl: user?.picture || '',
-                provider: "Auth0",
-                resetProfile: true,
-                uid: user?.sub || '',
+                provider: 'Auth0',
+                auth0: user?.sub || '',
+                blockedUsers: [], // List of people blocked by the user
+                posts: [], // List of posts the user has made
             }
             await setDoc(doc(db, "users", newAddedAuthRef.id), newUser);
-            // to create profile of the new user.
+
+            // Create a new user profile
             const newProfile: FirebaseProfile = {
-                bio: "",
+                bio: '',
                 dm: false,
-                lastName: user?.family_name as string || '',
-                location: "",
-                name: user.name || '',
+                lastName: '',
+                location: '',
+                resetProfile: true, // true since this is the default profile
+                name: '',
                 profilePic: user.picture || '',
-                resetProfile: false,
-                uid: user.sub || '',
                 username: user.nickname || ''
             }
             await setDoc(doc(db, "profiles", newAddedAuthRef.id), newProfile);
+
+            // Return the new profile
+            return newProfile;
         }
-        const getUserProfileData = await getDocs(query(collection(db, "users"), where("uid", "==", user.sub)));
-        return getUserProfileData.docs[0].data()
+        
+        // Profile data exists, fetch and return it
+        const profileId = checkIfUserExists.docs[0].id;
+        const userProfileDocRef = doc(db, "profiles", profileId);
+        const profileDocSnap = await getDoc(userProfileDocRef);
+        return profileDocSnap.data();
     } catch (e) {
         console.log(e)
     }
