454b10233e0b4ac262b583db20db62d88d9c6904
Evan Jones <evan.a.jones3@gmail.com>
2022-02-11 00:15:11 -0500

    removed auth0 collection without altering logic; fixed typos; staged the app for launching the UserProfileForm on first log-in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import {ThemeProvider} from 'next-themes'
-import type {AppProps} from 'next/app'
-import {RecoilRoot} from 'recoil'
-
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
 // Auth0
-import {UserProvider} from '@auth0/nextjs-auth0';
+import { UserProvider } from '@auth0/nextjs-auth0'
 
-function MyApp({Component, pageProps}: AppProps) {
-    // Read in config variables from environment
-    const appConfig = {
-        isDevelopment: process.env.NEXT_PUBLIC_DEV,
-    }
-
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider attribute="class" enableSystem={true}>
             <RecoilRoot>
