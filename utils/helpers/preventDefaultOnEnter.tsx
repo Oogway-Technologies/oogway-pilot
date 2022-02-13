996cb4d758021660597046b50a91548cdb8b93f0
Evan Jones <evan.a.jones3@gmail.com>
2022-02-12 19:27:53 -0500

    update prevent default on enter; fix dangling type declaration in FlashErrorMessage props

--- utils/helpers/preventDefaultOnEnter.tsx
+++ utils/helpers/preventDefaultOnEnter.tsx
@@ -1,6 +1,9 @@
 // Prevents default on form inputs when user presses enter
 const preventDefaultOnEnter = (e) => {
-    e.target.keyCode === 13 && e.preventDefault();
+    if (e.key === 'Enter') {
+        e.preventDefault()
+    }
+    e.target.keyCode === 13 && e.preventDefault()
 }
 
-export default preventDefaultOnEnter;
+export default preventDefaultOnEnter
