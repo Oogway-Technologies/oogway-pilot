8c4781ed9cc0eb184f22d18ceb18e91891facbee
Ihtisham Arif <im.ihtisham.arif@gmail.com>
2022-02-16 10:52:11 +0500

    removed common file

--- styles/common.ts
+++
@@ -1,7 +0,0 @@
-export const toolTipStyles = {
-    mainDiv: "relative flex flex-col items-center group",
-    hoverDiv: "absolute bottom-0 flex flex-col items-center hidden mb-11 group-hover:flex",
-    hoverText: "rounded-[8px] relative z-10 p-1 text-xs " +
-        "text-primary bg-white dark:bg-neutralDark-500 shadow-lg border-solid border-[1px] border-neutral-300",
-    tip: "w-3 h-3 -mt-2 rotate-45 bg-white dark:bg-neutralDark-500 border-solid border-[1px] border-neutral-300"
-}
