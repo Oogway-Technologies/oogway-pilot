c48481c1a42e21f6c37b01eb82eb8c12ff5b51ce
Ihtisham Arif <im.ihtisham.arif@gmail.com>
2022-02-16 10:42:59 +0500

    styles changed to string

--- components/Utils/Tooltip.tsx
+++ components/Utils/Tooltip.tsx
@@ -1,6 +1,4 @@
 import React, {FC} from "react";
-import {useHover} from "../../hooks/useHover";
-import {toolTipStyles} from "../../styles/common";
 
 interface TooltipProps {
     children: JSX.Element;
@@ -11,17 +9,17 @@ export const Tooltip: FC<TooltipProps> = ({
                                               children,
                                               toolTipText,
                                           }: TooltipProps) => {
-    const [hoverRef, isHovered] = useHover<HTMLDivElement>();
 
     return (
-        <div className={toolTipStyles.mainDiv}>
-            {isHovered && (
-                <div className={toolTipStyles.hoverDiv}>
-                    <span className={toolTipStyles.hoverText} style={{width: 'max-content'}}>{toolTipText}</span>
-                    <div className={toolTipStyles.tip}/>
-                </div>
-            )}
-            <div ref={hoverRef}>{children}</div>
+        <div className={'relative flex flex-col items-center group'}>
+            {children}
+            <div className={'absolute bottom-0 flex flex-col items-center hidden mb-11 group-hover:flex'}>
+                <span
+                    className={'w-max rounded-[8px] relative z-10 p-1 text-xs text-primary bg-white dark:bg-neutralDark-500 shadow-lg border-solid border-[1px] border-neutral-300'}
+                >{toolTipText}</span>
+                <div
+                    className={'w-3 h-3 -mt-2 rotate-45 bg-white dark:bg-neutralDark-500 border-solid border-[1px] border-neutral-300'}/>
+            </div>
         </div>
     );
 };
--- utils/helpers/common.ts
+++ utils/helpers/common.ts
@@ -1,3 +1,4 @@
+// count number of like in snapshot
 export const findLikes = (snapshot: any, setNumLikes: (n: number) => void) => {
     if (snapshot.data()) {
         // Get the likes map
@@ -12,4 +13,18 @@ export const findLikes = (snapshot: any, setNumLikes: (n: number) => void) => {
         });
         setNumLikes(ctr);
     }
+}
+
+// check wether the uploaded file is less then 10MB
+export const checkFileSize = (files: FileList | null) => {
+    if (!files) { // This is VERY unlikely, browser support is near-universal
+        console.error("This browser doesn't seem to support the `files` property of file inputs.");
+    }
+    if (files && files[0]) {
+        // checking if file is less then 10MB
+        if (files[0].size < 1000000) {
+            return true
+        }
+    }
+    return false
 }
