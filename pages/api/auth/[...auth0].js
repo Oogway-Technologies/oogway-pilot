53a1e23cd4f42f4cb5b482f83ae8cef06d79e2aa
Federico Campeotto <fede.campe@gmail.com>
2022-02-19 10:14:34 -0500

    Authenticate user with custom token using Firebase Auth.
    Custom token is generated given Auth0 authentication.

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,6 +1,8 @@
-import { handleAuth, handleCallback, getAccessToken } from '@auth0/nextjs-auth0'
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0'
+import { getAuth, signInWithCustomToken } from 'firebase/auth'
 
 const setFirebaseCustomToken = async (token) => {
+    // Fetchm the Firebase custom token from the Auth0 user
     const response = await fetch(
         'https://us-central1-oogway-pilot.cloudfunctions.net/getFirebaseToken',
         {
@@ -9,16 +11,38 @@ const setFirebaseCustomToken = async (token) => {
             },
         }
     )
+    return response.json()
 }
 
 const afterCallback = async (req, res, session, state) => {
-    console.log('after callback!')
-    const accessToken = await getAccessToken(req, res)
-    console.log('after token!', accessToken)
-    //const firebaseResponse = await setFirebaseCustomToken(accessToken)
-    //console.log('firebaseResponse:', firebaseResponse)
-    // delete session.refreshToken
+    // Retrieve the Firebase custom token from the Auth0 user
+    const firebaseResponse = await setFirebaseCustomToken(session.idToken)
 
+    // Sign in to Firebase with the custom token
+    const auth = getAuth()
+    const userCredential = await signInWithCustomToken(
+        auth,
+        firebaseResponse.firebaseToken
+    ).catch((error) => {
+        // TODO: block signing in?
+        const errorMessage = error.message
+        console.log('error:', errorMessage)
+    })
+
+    // Note: session.user already contains the Auth0 user
+    // as Session.user.sub. This should be the same as
+    // the userCredentials returned by Firebase, i.e.,
+    // if (userCredential) {
+    //    session.user.sub === userCredential.user.uid
+    // }
+    // TODO: check and/or assert the above condition?
+    if (userCredential && session.user.sub !== userCredential.user.uid) {
+        console.log(
+            'Mismatch between Auth0 user and Firebase auth user. This could be serious.'
+        )
+    }
+
+    // Return the session
     return session
 }
 
