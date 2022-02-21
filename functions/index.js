b4749b82b5674673003f3cf9840d07de91ba514d
Federico Campeotto <fede.campe@gmail.com>
2022-02-21 14:10:10 -0500

    Temp. commit delete function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
+// Token imports
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const jwt = require('jsonwebtoken')
 const jwks = require('jwks-rsa')
+
+// Firebase tools
+const firebase_tools = require('firebase-tools')
 
 const client = jwks({
     rateLimit: true,
@@ -65,3 +69,47 @@ exports.getFirebaseToken = functions.https.onRequest(async (req, res) => {
         return res.json({ firebaseToken })
     })
 })
+
+// [START recursive_delete_function]
+/**
+ * Initiate a recursive delete of documents at a given path.
+ *
+ * The calling user must be authenticated and have the custom "admin" attribute
+ * set to true on the auth token.
+ *
+ * This delete is NOT an atomic operation and it's possible
+ * that it may fail after only deleting some documents.
+ *
+ * @param {string} data.path the document or collection path to delete.
+ */
+exports.recursiveDelete = functions
+    .runWith({
+        timeoutSeconds: 540,
+        memory: '2GB',
+    })
+    .https.onCall(async (data, context) => {
+        // Only allow admin users to execute this function.
+        if (!context.auth) {
+            throw new functions.https.HttpsError('permission-denied')
+        }
+
+        const path = data.path
+        console.log(
+            `User ${context.auth.uid} has requested to delete path ${path}`
+        )
+
+        // Run a recursive delete on the given document or collection path.
+        // The 'token' must be set in the functions config, and can be generated
+        // at the command line by running 'firebase login:ci'.
+        await firebase_tools.firestore.delete(path, {
+            project: process.env.GCLOUD_PROJECT,
+            recursive: true,
+            yes: true,
+            token: functions.config().fb.token,
+        })
+
+        return {
+            path: path,
+        }
+    })
+// [END recursive_delete_function]
