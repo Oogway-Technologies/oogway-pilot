83cee18f741aa6e1208a4c6b1fd776e91f5ff096
Evan Jones <evan.a.jones3@gmail.com>
2022-03-05 00:46:02 -0500

    fix prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,9 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
+    printWidth: 80,
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    arrowParens: "avoid"
 };
