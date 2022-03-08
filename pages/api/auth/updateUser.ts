3cc3d7cc7294e761b19efe02cfa9cd8615aca7f6
Ihtisham Arif <im.ihtisham.arif@gmail.com>
2022-03-08 15:42:43 +0500

    change user id and req body type.

--- pages/api/auth/updateUser.ts
+++ pages/api/auth/updateUser.ts
@@ -1,4 +1,8 @@
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import {
+    getAccessToken,
+    getSession,
+    withApiAuthRequired,
+} from '@auth0/nextjs-auth0'
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from '../../../axios'
 
@@ -12,27 +16,29 @@ export default withApiAuthRequired(async function updateUser(
             res.status(405).send({ message: 'Only PATCH requests allowed' })
             return
         }
-        // Fetch request body
-        const body = { user_metadata: req.body }
+
         // Fetch user token
         const session = getSession(req, res)
+        const { accessToken } = await getAccessToken(req, res)
         const {
-            accessToken,
             user: { sub },
-        } = session ? session : { accessToken: '', user: { sub: '' } }
+        } = session ? session : { user: { sub: '' } }
+
         // Call management API
         const baseURL =
             process.env.AUTH0_ISSUER_BASE_URL?.indexOf('http') === 0
                 ? process.env.AUTH0_ISSUER_BASE_URL.replace('https', 'http')
                 : process.env.AUTH0_ISSUER_BASE_URL
-        const reqURL = baseURL + `api/v2/users/${sub.replace('auth0|', '')}`
+
+        const reqURL = baseURL + `api/v2/users/${sub}`
+
         const options = {
             headers: {
                 authorization: `Bearer ${accessToken}`,
                 'content-type': 'application/json',
             },
         }
-        const response = await axios.patch(reqURL, body, options)
+        const response = await axios.patch(reqURL, req.body, options)
 
         // Return
         res.status(response.status || 200).json(response.data)
