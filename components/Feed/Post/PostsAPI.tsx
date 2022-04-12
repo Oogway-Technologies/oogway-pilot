ce5c00f7e5238fcadae6003ee82cf31d5b13b95d
Evan Jones <evan.a.jones3@gmail.com>
2022-04-12 13:23:51 -0400

    hide demo account posts

--- components/Feed/Post/PostsAPI.tsx
+++ components/Feed/Post/PostsAPI.tsx
@@ -5,6 +5,10 @@ import useIntersectionObserver from '../../../hooks/useIntersectionObserver'
 import { useAppSelector } from '../../../hooks/useRedux'
 // Queries
 import { useInfinitePostsQuery } from '../../../queries/posts'
+import {
+    demoAccountIdDev,
+    demoAccountIdProd,
+} from '../../../utils/constants/global'
 import { FirebasePost } from '../../../utils/types/firebase'
 import {
     GeneratePostCardLoaders,
@@ -15,6 +19,9 @@ import EndOfFeedMessage from '../../Utils/EndOfFeedMessage'
 import PostCard from './Post'
 
 function PostsAPI() {
+    // Track user
+    const userProfile = useAppSelector(state => state.userSlice.user)
+
     // Instantiate infinite posts query
     const feed = useAppSelector(state => state.utilsSlice.feedState)
     const { status, data, isFetchingNextPage, fetchNextPage, hasNextPage } =
@@ -28,6 +35,14 @@ function PostsAPI() {
         onIntersect: fetchNextPage,
         enabled: !!hasNextPage,
     })
+
+    // Only allow demo account to see demo account posts
+    const isDemoAccountPost = (uid: string) => {
+        return uid === demoAccountIdDev || uid === demoAccountIdProd
+    }
+    const isDemoAccountUser = (uid: string) => {
+        return uid === demoAccountIdDev || uid === demoAccountIdProd
+    }
 
     return (
         <>
@@ -45,24 +60,37 @@ function PostsAPI() {
                         <Fragment key={page?.lastTimestamp?.seconds}>
                             {/* If posts collection exists */}
                             {page.posts &&
-                                page.posts.map((post: FirebasePost) => (
-                                    <PostCard
-                                        key={post.id}
-                                        id={post?.id || ''}
-                                        authorUid={post.uid}
-                                        name={post.name}
-                                        message={post.message}
-                                        description={post.description}
-                                        feed={post.feed || undefined}
-                                        isCompare={post.isCompare}
-                                        timestamp={post.timestamp}
-                                        postImage={post.postImage}
-                                        comments={null}
-                                        isCommentThread={false}
-                                        previewImage={post?.previewImage || ''}
-                                        isAnonymous={post?.isAnonymous || false}
-                                    />
-                                ))}
+                                page.posts.map((post: FirebasePost) => {
+                                    // Only allow demo account to see demo account posts
+                                    if (
+                                        isDemoAccountPost(post.uid) &&
+                                        !isDemoAccountUser(userProfile.uid)
+                                    )
+                                        return
+
+                                    return (
+                                        <PostCard
+                                            key={post.id}
+                                            id={post?.id || ''}
+                                            authorUid={post.uid}
+                                            name={post.name}
+                                            message={post.message}
+                                            description={post.description}
+                                            feed={post.feed || undefined}
+                                            isCompare={post.isCompare}
+                                            timestamp={post.timestamp}
+                                            postImage={post.postImage}
+                                            comments={null}
+                                            isCommentThread={false}
+                                            previewImage={
+                                                post?.previewImage || ''
+                                            }
+                                            isAnonymous={
+                                                post?.isAnonymous || false
+                                            }
+                                        />
+                                    )
+                                })}
                         </Fragment>
                     ))}
 
