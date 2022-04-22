05350fb244c25c25f1c21483df71fc22daa4ca4c
Evan Jones <evan.a.jones3@gmail.com>
2022-04-22 19:45:06 -0400

    debug failed ai bot calls

--- pages/api/ai/adviceBot.ts
+++ pages/api/ai/adviceBot.ts
@@ -45,116 +45,117 @@ async function handlePost(req: ExtendedNextApiRequest, res: NextApiResponse) {
             .json({ err: 'Client unauthorized to make this call.' })
     }
 
-    // try {
-    const { body } = req
+    try {
+        const { body } = req
 
-    // Create OpenAI Completion inputt
-    const post: FirebasePost = {
-        ...body,
-        timestamp: serverTimestamp(),
-    }
+        // Create OpenAI Completion inputt
+        const post: FirebasePost = {
+            ...body,
+            timestamp: serverTimestamp(),
+        }
 
-    // Send post info to Bing API
-    const params = {
-        q: post.message + ' site:reddit.com',
-        mkt: 'en-US',
-    }
-    const bingResponse = await BingSearchAPI.get('/', { params: params })
-    const bingData = bingResponse.data
+        // Send post info to Bing API
+        const params = {
+            q: post.message + ' site:reddit.com',
+            mkt: 'en-US',
+        }
+        const bingResponse = await BingSearchAPI.get('/', { params: params })
+        const bingData = bingResponse.data
 
-    // Get array of top N results
-    const references = bingData
-        ? (bingData.webPages.value.slice(0, bingTopN) as BingReference[])
-        : null
+        // Get array of top N results
+        const references = bingData
+            ? (bingData.webPages.value.slice(0, bingTopN) as BingReference[])
+            : null
 
-    // Send post info to Oogway Decision endpoint
-    const decisionData = {
-        question: post.message,
-        context: post.description, // Does this need to be truncated
-        token: process.env.OOGWAY_DECISION_TOKEN,
-    }
-    const decisionResponse = await OogwayDecisionAPI.post(
-        'oogway_decision',
-        decisionData
-    )
-    const responseData = decisionResponse.data
-
-    if (decisionResponse.status !== 200) {
-        throw new Error(
-            `Oogway Decision API failed with (${res.status}): ${
-                typeof responseData === 'string'
-                    ? responseData
-                    : JSON.stringify(responseData, null, 2)
-            }`
+        // Send post info to Oogway Decision endpoint
+        const decisionData = {
+            question: post.message,
+            context: post.description, // Does this need to be truncated
+            token: process.env.OOGWAY_DECISION_TOKEN,
+        }
+        const decisionResponse = await OogwayDecisionAPI.post(
+            'oogway_decision',
+            decisionData
         )
-    }
+        const responseData = decisionResponse.data
 
-    // Extract advice bot message and filter status
-    const adviceBotMessage = responseData['decision']
-    const filterStatus = responseData['label']
+        if (decisionResponse.status !== 200) {
+            throw new Error(
+                `Oogway Decision API failed with (${res.status}): ${
+                    typeof responseData === 'string'
+                        ? responseData
+                        : JSON.stringify(responseData, null, 2)
+                }`
+            )
+        }
 
-    // Log activity to 'advice-bot-calls' collection
-    const adviceBotCall: OpenAPICall = {
-        postId: post.id,
-        postMessage: post.message,
-        postDescription: post.description,
-        authorId: post.uid,
-        feed: post.feed,
-        completionPrompt: responseData.prompt,
-        completionHyperparams: responseData.gpt3_params,
-        completionResponse: adviceBotMessage,
-        filterPrompt: responseData.filter_prompt,
-        filterResponse: filterStatus,
-        filterLogprobs: responseData.probs,
-        timestamp: serverTimestamp(),
-    }
-    await addDoc(collection(db, 'advice-bot-calls'), adviceBotCall)
+        // Extract advice bot message and filter status
+        const adviceBotMessage = responseData['decision']
+        const filterStatus = responseData['label']
 
-    // Create advice bot comment
-    const adviceBotProfile = await getDoc(doc(db, 'profiles', adviceBotId))
-    const sensitiveDataMessage =
-        'Sorry, our AI does not have any advice for you. Our decision coaches can help you with this decision.'
-    const adviceBotComment: AIBotComment = {
-        postId: post.id,
-        parentId: null,
-        isComment: true,
-        timestamp: serverTimestamp(),
-        message: filterStatus === '2' ? sensitiveDataMessage : adviceBotMessage,
-        references: references,
-        author: adviceBotProfile?.data()?.name || 'Oogway AI Bot',
-        authorUid: adviceBotId,
-        likes: {},
-        dislikes: {},
-        filterStatus: filterStatus,
-    }
-    const docRef = await addDoc(
-        collection(db, `post-activity`),
-        adviceBotComment
-    )
-    if (!docRef) {
-        return res
-            .status(403)
-            .json({ err: 'Failed to create advice bot comment' })
-    }
+        // Log activity to 'advice-bot-calls' collection
+        const adviceBotCall: OpenAPICall = {
+            postId: post.id,
+            postMessage: post.message,
+            postDescription: post.description,
+            authorId: post.uid,
+            feed: post.feed,
+            completionPrompt: responseData.prompt,
+            completionHyperparams: responseData.gpt3_params,
+            completionResponse: adviceBotMessage,
+            filterPrompt: responseData.filter_prompt,
+            filterResponse: filterStatus,
+            filterLogprobs: responseData.probs,
+            timestamp: serverTimestamp(),
+        }
+        await addDoc(collection(db, 'advice-bot-calls'), adviceBotCall)
 
-    // Add notification for user
-    const engagemment: FirebaseEngagement = {
-        engagerId: adviceBotId,
-        engageeId: post.uid,
-        action: 'comment',
-        targetId: docRef.id,
-        targetObject: 'Post',
-        targetRoute: `comments/${post.id}`,
-        isNew: true,
-        timestamp: serverTimestamp(),
-    }
-    await addDoc(collection(db, 'engagement-activity'), engagemment)
+        // Create advice bot comment
+        const adviceBotProfile = await getDoc(doc(db, 'profiles', adviceBotId))
+        const sensitiveDataMessage =
+            'Sorry, our AI does not have any advice for you. Our decision coaches can help you with this decision.'
+        const adviceBotComment: AIBotComment = {
+            postId: post.id,
+            parentId: null,
+            isComment: true,
+            timestamp: serverTimestamp(),
+            message:
+                filterStatus === '2' ? sensitiveDataMessage : adviceBotMessage,
+            references: references,
+            author: adviceBotProfile?.data()?.name || 'Oogway AI Bot',
+            authorUid: adviceBotId,
+            likes: {},
+            dislikes: {},
+            filterStatus: filterStatus,
+        }
+        const docRef = await addDoc(
+            collection(db, `post-activity`),
+            adviceBotComment
+        )
+        if (!docRef) {
+            return res
+                .status(403)
+                .json({ err: 'Failed to create advice bot comment' })
+        }
 
-    // Return successful comment
-    return res.status(201).json(adviceBotComment)
-    // } catch (err) {
-    //     return res.status(403).json({ err: 'Error!' })
-    // }
+        // Add notification for user
+        const engagemment: FirebaseEngagement = {
+            engagerId: adviceBotId,
+            engageeId: post.uid,
+            action: 'comment',
+            targetId: docRef.id,
+            targetObject: 'Post',
+            targetRoute: `comments/${post.id}`,
+            isNew: true,
+            timestamp: serverTimestamp(),
+        }
+        await addDoc(collection(db, 'engagement-activity'), engagemment)
+
+        // Return successful comment
+        return res.status(201).json(adviceBotComment)
+    } catch (err) {
+        return res.status(403).json({ err: err })
+    }
 }
 
 export default async function adviceBotHandler(
