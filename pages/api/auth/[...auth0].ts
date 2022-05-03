bd7ba4fab67e6cb3fda13f4d4fe5c0cfe5ecf06b
Yosgi <18667911647yosgi@gmail.com>
2022-05-03 02:27:45 +0000

    Enable choose a different login method after declining sign in

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -78,7 +78,11 @@ export default handleAuth({
                 afterCallback,
             })
         } catch (err) {
-            res.setHeader('location', '/')
+            res.redirect(
+                `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?${new URLSearchParams({
+                  client_id:process.env.AUTH0_CLIENT_ID  as string,
+                })}`
+              )
             .status(302)
             .end();
         }
