2fdf5b3d2e0caef339230283b88a4932539ab7e9
Evan Jones <evan.a.jones3@gmail.com>
2022-05-19 18:50:45 -0400

    log error and userId in registerEmail

--- pages/api/registerEmail.ts
+++ pages/api/registerEmail.ts
@@ -6,6 +6,7 @@ export default async function handleGet(
     res: NextApiResponse
 ) {
     const userId = req?.query?.userId || ''
+    console.log('UserId query param: ', userId) // remove
     if (!userId) {
         return res.status(400).json({
             error: 'no user id in query params',
@@ -20,12 +21,13 @@ export default async function handleGet(
 
     management.getUser({ id: userId as string }, function (error: Error, user) {
         if (error) {
+            console.log('Error: ', error)
             res.status(400).json({
                 error: 'error while fetching user email from Auth0',
             })
             return
         }
-        console.log('User from within getUser callback: ', user) // remove
+
         res.status(200).json({ email: user.email })
     })
 }
