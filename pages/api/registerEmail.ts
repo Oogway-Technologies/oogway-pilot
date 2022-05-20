98d49ebd7e78dab1bfab93bf691050c01ada7e4a
Evan Jones <evan.a.jones3@gmail.com>
2022-05-20 14:36:10 -0400

    remove console logs

--- pages/api/registerEmail.ts
+++ pages/api/registerEmail.ts
@@ -6,7 +6,6 @@ export default async function handleGet(
     res: NextApiResponse
 ) {
     const userId = req?.query?.userId || ''
-    console.log('UserId query param: ', userId) // remove
     if (!userId) {
         return res.status(400).json({
             error: 'no user id in query params',
@@ -21,13 +20,12 @@ export default async function handleGet(
 
     management.getUser({ id: userId as string }, function (error: Error, user) {
         if (error) {
-            console.log('Error: ', error)
             res.status(400).json({
                 error: 'error while fetching user email from Auth0',
             })
             return
         }
-
         res.status(200).json({ email: user.email })
+        return
     })
 }
