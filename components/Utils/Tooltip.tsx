54da649cb870009e4a0d2b4e2bd96bb1507339f3
Ihtisham Arif <im.ihtisham.arif@gmail.com>
2022-06-23 01:20:42 +0500

    added tooltip

--- components/Utils/Tooltip.tsx
+++ components/Utils/Tooltip.tsx
@@ -3,30 +3,37 @@ import React, { FC } from 'react'
 interface TooltipProps {
     children: JSX.Element
     toolTipText: string
+    className?: string
+    classForParent?: string
+    classForToolTipBox?: string
+    classForBottomArrow?: string
 }
 
 export const Tooltip: FC<
     React.PropsWithChildren<React.PropsWithChildren<TooltipProps>>
-> = ({ children, toolTipText }: TooltipProps) => {
+> = ({
+    children,
+    toolTipText,
+    className = '',
+    classForParent = '',
+    classForToolTipBox = '',
+    classForBottomArrow = '',
+}: TooltipProps) => {
     return (
-        <div className={'group flex relative flex-col items-center'}>
+        <div
+            className={`group flex relative flex-col items-center ${className}`}
+        >
             {children}
             <div
-                className={
-                    'hidden group-hover:flex absolute bottom-0 flex-col items-center mb-11'
-                }
+                className={`hidden group-hover:flex absolute bottom-0 flex-col items-center mb-11 ${classForParent}`}
             >
                 <span
-                    className={
-                        'relative z-10 p-1 w-max text-xs text-primary bg-white dark:bg-neutralDark-500 rounded-[8px] border-[1px] border-neutral-300 border-solid shadow-lg'
-                    }
+                    className={`relative z-10 p-1 w-max text-xs text-primary bg-white dark:bg-neutralDark-500 rounded-[8px] border-[1px] border-neutral-300 border-solid shadow-lg ${classForToolTipBox}`}
                 >
                     {toolTipText}
                 </span>
                 <div
-                    className={
-                        '-mt-2 w-3 h-3 bg-white dark:bg-neutralDark-500 border-[1px] border-neutral-300 border-solid rotate-45'
-                    }
+                    className={`-mt-2 w-3 h-3 bg-white dark:bg-neutralDark-500 border-[1px] border-neutral-300 border-solid rotate-45 ${classForBottomArrow}`}
                 />
             </div>
         </div>
