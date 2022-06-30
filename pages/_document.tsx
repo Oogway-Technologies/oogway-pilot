5a0bf4f2dd9eea0bbef1ed74784bb8b5fbb078c4
Evan Jones <evan.a.jones3@gmail.com>
2022-06-29 21:14:48 -0400

    remove duplicate meta tag _document due to warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,10 +21,6 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1, maximum-scale=1"
-                    />
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `(function(h,o,t,j,a,r){
